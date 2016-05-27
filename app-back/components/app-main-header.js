79694cd9f185faeb7d2885cce0f6763247cdaf03
ubuntuvim <1527254027@qq.com>
2016-05-27 23:19:49 +0800

    删除备份的文件app-back

--- app-back/components/app-main-header.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-    classNames: [''],
-    attributeBindings: ['id'],
-    id: 'appMainHeaderId'
-});
--- app-back/components/script-tag.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-// ember2.0之后不支持在模板中使用script标签，
-// 通过组件方式加入
-export default Ember.Component.extend({
-	tagName: 'script',
-    attributeBindings: ['type'],
-    type: 'text/javascript'
-});

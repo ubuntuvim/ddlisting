80e5ff82c4a5c2668df880cf9ebd60befc369c23
ubuntuvim <1527254027@qq.com>
2016-06-12 21:36:08 +0800

    使用firebase时不需要序列化器

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,27 +1,27 @@
-// app/adapters/application.js
-import Ember from 'ember';
-
-import FirebaseAdapter from 'emberfire/adapters/firebase';
-
-const { inject } = Ember;
-
-// 使用firebase实时数据库
-export default FirebaseAdapter.extend({
-  firebase: inject.service()
-});
-
-// import JSONAPIAdapter from 'ember-data/adapters/json-api';
+// // app/adapters/application.js
+// import Ember from 'ember';
 //
-// export default JSONAPIAdapter.extend({
-//     host: 'http://localhost:3000'
-//     // host: 'https://ddlisting.wilddogio.com',
-//   //   ajaxOptions: function(url, type, options) {
-//   //       var hash = this._super(url, type, options);
-//   //       hash.method = "post";
-//   //       return hash;
-//   //   },
-//   //   headers: {
-//   //       'API_KEY': 'secret key',
-//   //       'ANOTHER_HEADER': 'Some header value'
-//   // }
+// import FirebaseAdapter from 'emberfire/adapters/firebase';
+//
+// const { inject } = Ember;
+//
+// // 使用firebase实时数据库
+// export default FirebaseAdapter.extend({
+//   firebase: inject.service()
 // });
+
+import JSONAPIAdapter from 'ember-data/adapters/json-api';
+
+export default JSONAPIAdapter.extend({
+    host: 'http://localhost:3000'
+    // host: 'https://ddlisting.wilddogio.com',
+  //   ajaxOptions: function(url, type, options) {
+  //       var hash = this._super(url, type, options);
+  //       hash.method = "post";
+  //       return hash;
+  //   },
+  //   headers: {
+  //       'API_KEY': 'secret key',
+  //       'ANOTHER_HEADER': 'Some header value'
+  // }
+});

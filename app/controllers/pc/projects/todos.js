355799807d746ff6288ce6be544aa2e7c53f5c48
ubuntuvim <1527254027@qq.com>
2016-06-30 02:08:35 +0800

    完成查询功能，并在主列表底部增加一个工具条，不过还没实现其功能

--- app/controllers/pc/projects/todos.js
+++ app/controllers/pc/projects/todos.js
@@ -8,4 +8,21 @@ export default Ember.Controller.extend({
     queryTodoValueCpt: Ember.computed('queryTodoValue', function() {
         return this.get('queryTodoValue');
     }),
+    // 多重过滤：1，状态为1；2，登录用户id；3，所属分类；4，不显示子todo
+    completedCount: Ember.computed('todosForTotla.@each.userId',
+                                        'todosForTotla.@each.recordStatus',
+                                        'todosForTotla.@each.project',
+                                        'todosForTotla.@each.isChildOrParent', 'model', function() {
+
+        // var userId = sessionStorage.getItem("__LOGIN_USER_ID__");
+        // var projectId = this.get('projectId');  //调用组件时候传递过来
+        //
+        // return this.get('todosForTotla').filter(function(td) {
+        //     return td.get('userId') === userId
+        //             && td.get('recordStatus') === 1
+        //             && td.get('project').get('id') === projectId
+        //             && td.get('isChildOrParent') === 3;
+        // }).get('length');
+        return 10;
+    })
 });
---
+++ tests/unit/helpers/completed-count-test.js
@@ -0,0 +1,10 @@
+import { completedCount } from 'ddlisting/helpers/completed-count';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | completed count');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = completedCount([42]);
+  assert.ok(result);
+});

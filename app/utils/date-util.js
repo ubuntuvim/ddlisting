ad0173441c63b756f2bf6e9ef6a63404f37f4e5f
ubuntuvim <1527254027@qq.com>
2016-06-29 14:10:50 +0800

    共同方法，获取当前时间

---
+++ app/utils/date-util.js
@@ -0,0 +1,9 @@
+// 获取当前时间，时间格式为：2016-06-29 10:06
+export default function dateUtil() {
+    var date = new Date(new Date());
+    return date.getFullYear() + '-'
+        + (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-'
+        + date.getDate() + ' '
+        + date.getHours() + ':'
+        + date.getMinutes();
+}

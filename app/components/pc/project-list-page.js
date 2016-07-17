f8cdb3193b3072f93ce1eaceb923cda0fd235b7a
ubuntuvim <1527254027@qq.com>
2016-07-17 23:56:50 +0800

    完成help页面，修复左侧分类出现滚动条问题（增加inner层）

--- app/components/pc/project-list-page.js
+++ app/components/pc/project-list-page.js
@@ -1,14 +1,26 @@
+/**
+* @Author: ubuntuvim
+* @Date:   2016-06-28T21:08:17+08:00
+* @Last modified by:   ubuntuvim
+* @Last modified time: 2016-07-17T23:39:41+08:00
+*/
+
+
+
 // app/components/project-list-page.js
 import Ember from 'ember';
 
 // 左侧项目分类列表
 export default Ember.Component.extend({
+	didInsertElement() {
+        // Ember.$("#appMainLeftId .panel .panel-body .list-group").css("height", $(window).width()); 
+	},
 	allProject: Ember.computed(function() {
         return this.store.findAll('project');
     }),
     projects: Ember.computed('allProject.@each.userId',
         					'allProject.@each.projStatus', function() {
-        
+
         // console.log("this.get('allTodos') --- " + this.get('allTodos'));
         let userId = sessionStorage.getItem("__LOGIN_USER_ID__");
 
--- app/routes/help.js
+++ app/routes/help.js
@@ -1,4 +1,34 @@
+/**
+* 项目介绍页面路由 app/routes/help.js
+* @Author: ubuntuvim
+* @Date:   2016-07-15T00:52:03+08:00
+* @Last modified by:   ubuntuvim
+* @Last modified time: 2016-07-17T22:27:16+08:00
+*/
 import Ember from 'ember';
+import getUserId from '../utils/get-user-id';
+import config from '../config/environment';
+// import logout from '../utils/logout';
 
 export default Ember.Route.extend({
+    // 判断用户是否登录了，没有登录不允许进入
+    redirect(model, transition) {
+        if (!getUserId()) {
+            this.transitionTo('help');
+        }
+    },
+    model() {
+        return Ember.RSVP.hash({
+            loginUser: getUserId(),
+            user: this.store.peekRecord('user', getUserId())
+        });
+    },
+    actions: {
+        logout() {
+            sessionStorage.removeItem(config.APP.__LOGIN_USER_ID__);
+            Ember.$.cookie(config.APP.__LOGIN_USER_ID__, '', { expires: -1 }); // 删除 cookie
+            location.reload();  //在help页面退出需要强制刷新页面
+            // logout();
+        }
+    }
 });

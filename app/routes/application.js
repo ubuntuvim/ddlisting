aa4da40ba217e3f4705857cbfecd4198614b52d7
ubuntuvim <1527254027@qq.com>
2016-08-10 01:58:38 +0800

    删除不必要的loading图片，修复application路由跳转问题：不同设备跳转不同页面，刷新不跳转到其他页面

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,17 +4,35 @@
 * @Author: ubuntuvim
 * @Date:   2016-05-26T22:15:56+08:00
 * @Last modified by:   ubuntuvim
-* @Last modified time: 2016-08-05T03:08:40+08:00
+* @Last modified time: 2016-08-10T01:56:04+08:00
 */
 import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-    redirect(model, transition) {
-        this.transitionTo('pc.publicTodos');
+    // redirect(model, transition) {
+    //     this.transitionTo('pc.publicTodos');
+    // },
+
+    beforeModel() {
+        let hostname = "http://" + window.location.host + "/";
+        // 首次访问时候判断是手机还是PC端分别进入不同的处理页面
+        if (hostname == window.location.href || (hostname + "#/") == window.location.href) {  //当前窗口的URL与首页一致（http://localhost:4200)
+            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+                Ember.Logger.debug("移动设备");
+                this.transitionTo('m.projects');
+            } else {
+                Ember.Logger.debug("PC端");
+                this.transitionTo('pc.publicTodos');
+            }
+        } else {  //如果已经进入到某个路由，并且手动刷新页面则进入到刷新时候的页面
+            // window.location.href获取到当前窗口的URL
+            location.href = window.location.href;
+        }
     },
 
     model() {
+
         return Ember.RSVP.hash({
             // 不会在模板中获取这个数据，这里只是为了查询一次数据，是的后面再使用project的时候可以从缓存中获取
             // 否则手动刷新后页面不显示数据
